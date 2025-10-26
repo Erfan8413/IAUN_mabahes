c75e100dc50a5f488b5da839913fd83c4f064ca5
Erfan8413 <e.ashoori123@gmail.com>
2025-10-26 18:17:42 +0330

    exercise1

---
+++ ts.py
@@ -0,0 +1,8 @@
+years = int(input("Enter your age: "))
+days = years * 365
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+print("hour:", hours)
+print("minuts:" ,minutes)
+print("seconds:" , seconds)
